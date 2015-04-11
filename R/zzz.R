15ff108cf2161badf9585de867dd726aeaa9777a
Neal Fultz <nfultz@gmail.com>
2015-04-11 12:38:32 -0700

    Equipping tron

---
+++ R/zzz.R
@@ -0,0 +1,9 @@
+
+# This will activate tron on a whole package
+.onLoad <- function(libName,pkgName) {
+   if(getOption("tron", FALSE) && requireNamespace("tron", quietly=TRUE)) {
+     packageStartupMessage("Using tron on ", pkgName)
+     tron:::tron.environment(parent.env(environment()))
+   }
+}
+

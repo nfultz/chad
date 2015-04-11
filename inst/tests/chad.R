c2c1ce33db5a0ae7e5f2c2a1acee778b3e852205
Neal Fultz <nfultz@gmail.com>
2015-04-11 13:13:27 -0700

    Moving tests to test/chad/

---
+++ inst/tests/chad.R
@@ -0,0 +1,13 @@
+
+if(requireNamespace("chad", quietly=TRUE)) local({
+  wd <- getwd()
+  setwd("chad")
+  
+  if(!!chad("meta")) stop("metachad diff")
+
+
+  
+
+  
+
+}) else warning("Chadscripts not run")
